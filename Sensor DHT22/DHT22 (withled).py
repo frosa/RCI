5351402c21bc8b777d0a8953a776b3c7ec5e5b27
aecormar <38533958+aecormar@users.noreply.github.com>
2018-05-19 11:40:13 +0100

    Update and rename DHT22.py to DHT22 (withled).py

--- Sensor DHT22/DHT22 (withled).py
+++ Sensor DHT22/DHT22 (withled).py
@@ -4,16 +4,20 @@
 
 import dht
 import machine
+from machine import pin
 import time # Incluimos la función time para establecer los dos segundos de refresco establecidos por el fabricante para obtener los datos de la medición.
 sensor = dht.DHT22(machine.Pin(4)) # De esta manera indicamos que el sensor está conectado al pin 4 del módulo.
-
+LED = Pin(12, Pin.OUT) # Configuramos el pin 12 (podemos usar cualquier pin libre entre  0, 1, 2, 3, 4, 5, 12, 13, 14, 15, 16) como pin controlable por la variable LED donde se conecta el LED.
+LED.off() # Iniciamos en low la salida del pin correspondiente al LED
 # Dentro del Loop Main y cada vez que se necesite adquirir los valores de humedad y temperatura se deben de usar las siguientes instrucciones
 
 # En este caso, se incluye un ejemplo de loop (bucle for) donde se toman los datos de temperatura y humedad 10 veces y se muestra a través de print.
 
 for i in range(10):
     sensor.measure()
+    LED.on() # Indicamos con el LED que estamos midiemdo la temperatura y humedad
     time.sleep_ms(2000)
+    LED.off() # Finalizamos la medición
     print('temperatura grados celsius:', end=' ')
     print(sensor.temperature(), end=' ')
     print('Humedad reativa %:', end=' ')
